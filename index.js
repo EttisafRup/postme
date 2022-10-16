61a4f8991861fb3d2e114aa7668a37076e22e467
EttisafRup <ettisafrup@gmail.com>
2022-10-16 23:46:51 +0600

    bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const checkAdmin = require("./middlewares/checkAdmin");
-app.use("/image", checkAdmin, express.static("./uploads/"));
+app.use("/image", express.static("./uploads/"));
 
 // ?>>= Enable CORS
 const cors = require("cors");
